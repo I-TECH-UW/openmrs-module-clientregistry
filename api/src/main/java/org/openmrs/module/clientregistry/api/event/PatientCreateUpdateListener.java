c7714152e0df513c3a10ef700980744373d6b4c3
Piotr Mankowski <pmanko@uw.edu>
2022-09-19 21:36:03 -0700

    Send patient to CR

--- api/src/main/java/org/openmrs/module/clientregistry/api/event/PatientCreateUpdateListener.java
+++ api/src/main/java/org/openmrs/module/clientregistry/api/event/PatientCreateUpdateListener.java
@@ -1,12 +1,25 @@
 package org.openmrs.module.clientregistry.api.event;
 
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hl7.fhir.r4.model.HumanName;
+import org.hl7.fhir.r4.model.Patient;
+import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Daemon;
 import org.openmrs.event.EventListener;
 import org.openmrs.module.DaemonToken;
+import org.openmrs.module.clientregistry.exception.ClientRegistryException;
+import org.openmrs.module.fhir2.api.FhirPatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,7 +36,13 @@ public class PatientCreateUpdateListener implements EventListener {
 	}
 	
 	private DaemonToken daemonToken;
-	
+
+	@Autowired
+	private FhirPatientService patientService;
+
+	@Autowired
+	private IGenericClient client;
+
 	@Override
 	public void onMessage(Message message) {
 		log.trace("Received message: " + message);
@@ -39,6 +58,34 @@ public class PatientCreateUpdateListener implements EventListener {
 	}
 	
 	private void processMessage(Message message) {
+		if (message instanceof MapMessage) {
+			MapMessage mapMessage = (MapMessage) message;
+
+			String uuid;
+			try {
+				uuid = mapMessage.getString("uuid");
+				log.debug("Handling patient " + uuid);
+			}
+			catch (JMSException e) {
+				log.error("Exception caught while trying to get patient uuid for event", e);
+				return;
+			}
+
+			if (uuid == null || StringUtils.isBlank(uuid)) {
+				return;
+			}
+
+			Patient patient;
+			patient = patientService.get(uuid);
+			patient.getNameFirstRep().setUse(HumanName.NameUse.OFFICIAL);
+
+			MethodOutcome result = client.create().resource(patient).execute();
+
+			if (!result.getCreated()) {
+				log.error(String.format("Error creating or updating patient in Client Registry: %s",
+					result.getResource().getClass().getName()));
+			}
+		}
 	}
 	
 }
